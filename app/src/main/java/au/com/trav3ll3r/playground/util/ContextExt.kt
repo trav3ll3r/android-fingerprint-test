2e507eadb29fe7f4ab8663a4af8c1629d555e760
trav3ll3r <sinisa.poznic@gmail.com>
2017-08-25 16:26:02 +1000

    Implement statusBarHeight as Context extension property

---
+++ app/src/main/java/au/com/trav3ll3r/playground/util/ContextExt.kt
@@ -0,0 +1,13 @@
+package au.com.trav3ll3r.playground.util
+
+import android.content.Context
+
+val Context.statusBarHeight: Int
+    get() {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
