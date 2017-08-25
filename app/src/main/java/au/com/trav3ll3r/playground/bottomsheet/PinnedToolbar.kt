71504c7ce854f74d63510453d06203fb78b8a934
trav3ll3r <sinisa.poznic@gmail.com>
2017-08-25 10:17:27 +1000

    Remove unused classes

--- app/src/main/java/au/com/trav3ll3r/playground/bottomsheet/PinnedToolbar.kt
+++
@@ -1,9 +0,0 @@
-package au.com.trav3ll3r.playground.bottomsheet
-
-import android.content.Context
-import android.util.AttributeSet
-import android.widget.RelativeLayout
-
-class PinnedToolbar(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
-
-}
